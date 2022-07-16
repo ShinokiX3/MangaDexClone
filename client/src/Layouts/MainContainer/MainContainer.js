a18610362ef04538d38417fefb889b8ef134c464
Tsarenko R <endernasha85@gmail.com>
2022-07-16 18:41:54 +0300

    create new layout

---
+++ client/src/Layouts/MainContainer/MainContainer.js
@@ -0,0 +1,56 @@
+import React, { memo, useEffect, useRef, useState } from 'react';
+import { useObserver } from '../../Hooks/observer';
+
+const MainContainer = memo(({ children, mainClasses = '', containerClasses = '', isHeaderBlack = false }) => {
+    const [observerStatus, setObserverRef] = useObserver({threshold: 0});
+    
+    useEffect(() => {
+        const headerTemp = document.querySelector('.header-plug');
+        setObserverRef(headerTemp);        
+    }, []);
+
+    useEffect(() => {
+        if (!observerStatus) {
+            toggleStyles('add');
+        } else {
+            toggleStyles('remove');
+        }
+        return () => {
+            toggleStyles('remove');
+        }
+    }, [observerStatus]);
+
+    const toggleStyles = (type) => {
+        const header = document.querySelector('.header-block');
+
+        const addStyles = () => {
+            header?.classList.remove('header-white');
+            if (isHeaderBlack) {
+                header?.classList.add('header-transparent-black');
+            } else {
+                header?.classList.add('header-transparent');
+            }
+        }
+
+        const removeStyles = () => {
+            if (isHeaderBlack) {
+                header?.classList.remove('header-transparent-black');
+            } else {
+                header?.classList.remove('header-transparent');
+            }
+            header?.classList.add('header-white');
+        }
+        
+        if (type === "add") addStyles(); else removeStyles();
+    }
+
+    return (
+        <main className={mainClasses}>
+            <div className={containerClasses}>
+                { children }
+            </div>
+        </main>
+    );
+});
+
+export default MainContainer;
