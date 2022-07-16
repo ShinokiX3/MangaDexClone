c37773b901dc19decf69a81f0a8cb6855048b60d
Tsarenko R <endernasha85@gmail.com>
2022-07-16 18:45:30 +0300

    new observer functionality

--- client/src/Hooks/observer.js
+++ client/src/Hooks/observer.js
@@ -1,31 +1,32 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 export const useObserver = (options) => {
-    const [ref, setRef] = useState(null);
-    // const setRef = useRef(null);
-    const [stickyStatus, setstickyStatus] = useState(false);
+    const [observerRef, setObserverRef] = useState(null);
+    const [observerStatus, setObserveStatus] = useState(false);
     
     useEffect(() => {
         const observer = new IntersectionObserver(([entry]) => {
-            if (entry.isIntersecting) {
+            console.log(entry);
+            if (!entry.isIntersecting) {
                 console.log('out of');
-                setstickyStatus(true);
+                setObserveStatus(true);
             } else {
-                setstickyStatus(false);
+                console.log('inn');
+                setObserveStatus(false);
             }
         }, options);
 
-        if (ref) {
-            observer.observe(ref);
+        if (observerRef) {
+            observer.observe(observerRef);
         }
 
         return () => {
-            if (ref) {
-                observer.unobserve(ref);
+            if (observerRef) {
+                observer.unobserve(observerRef);
             }
         }
 
-    }, [ref, options]);
+    }, [observerRef, options]);
     
-    return [stickyStatus, setRef];
+    return [observerStatus, setObserverRef];
 }
