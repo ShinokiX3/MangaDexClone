dc799f4977effa4bb3328da6205cfa4258742316
Tsarenko R <endernasha85@gmail.com>
2022-07-25 23:13:16 +0300

    create separate view for filter item

---
+++ client/src/SharedUI/Filter/FilterItemsViev.js
@@ -0,0 +1,17 @@
+import styles from './filter-items.module.scss'
+
+const FilterItemsViev = ({ title = '', id = '', style = '', handleClick = null, cRef = null, include = false, exclude = false }) => {
+    return (
+        <span 
+            ref={cRef} 
+            className={`${styles.exemplar} ${style} ${include === true ? styles.include : ''} ${exclude === true ? styles.exclude : ''}`} 
+            onClick={handleClick} 
+            id={title.split(' ').join('-')}
+            data-id={id}
+        >
+            {title}
+        </span>
+    );
+};
+
+export default FilterItemsViev;
