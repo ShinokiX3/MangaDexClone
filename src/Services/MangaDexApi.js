2b40e15f7e449cec171afd69a041a1149eb73d67
shinoki <endernasha85@gmail.com>
2023-03-10 05:07:18 +0300

    cors proxy

--- src/Services/MangaDexApi.js
+++ src/Services/MangaDexApi.js
@@ -1,7 +1,7 @@
 import { filterSomeAttribute } from "../Utils/filterAttribute";
 
 class MangaDexApi {
-    CorsProxy = 'https://justcors.com/tl_f6b83a2/';
+    CorsProxy = 'https://justcors.com/tl_1c240dd/';
     Setting = '?forcePort443=false';
 
     BaseUrl = `${this.CorsProxy}https://api.mangadex.org`;
